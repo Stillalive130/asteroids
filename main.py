c9fcc2ef4e82ec40ab9db8927ab87af4a2b275af
Kris Nemirsky <krisnemirsky@gmail.com>
2024-10-06 17:28:44 -0700

    initial commit

--- main.py
+++ main.py
@@ -3,18 +3,31 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
 
 def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH/ 2, SCREEN_HEIGHT / 2)
+    dt = 0
+
     while True:
-        screen.fill(color="black")
-        pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        screen.fill(color="black")
+        player.draw(screen)
+        pygame.display.flip()
+
+        # Limit the framerate to 60 FPS
+        dt = clock.tick(60)/1000
+
+       
+                
 
 if __name__ =="__main__":
     main()
